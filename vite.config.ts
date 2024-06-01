a65347c60bec3e2c281ad19092676355dd19aa1f
nicolasadekz <nickpizzoni@outlook.com>
2024-06-01 17:02:51 -0300

    chore: initial commit with shadcn ui configs

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+//'node:' serve para prefixar que o modulo é importado do node
+import path from "node:path"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
